36fb6acbe73649f46b6ac62fa757ca2c8a9ce3f5
Oda <odalauten@gmail.com>
2015-05-02 22:04:14 +0200

    Music

--- testing/music/music.ino
+++ testing/music/music.ino
@@ -1,9 +1,27 @@
+/*
+Arduino
+
+Om fila, prosjektet
+
+*/
+
+/*********************************
+ *  IMPORTS
+ *********************************/
+
 #include <Wire.h>
+
 #include "pitches.h"
 
-int SOUND_PIN = 9;
+/*********************************
+ *  GLOBAL
+ *********************************/
+
+/* Connections */
+const int SOUND_PIN = 9;
 //int ENABLE_PIN = 2;
 
+/* Variables */
 unsigned long note_duration = 1000;    // in milliseconds
 int step_position;
 int current;
@@ -11,16 +29,26 @@ int current;
 // note frequencies corresponding to the 12 different laser beams
 int frequencies[] = {NOTE_A3, NOTE_B3, NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4, NOTE_C5, NOTE_D5, NOTE_E5};
 
+
+/**********************************
+
+
+/**
+ * Setup
+ */ 
 void setup(void) {
     //Serial.begin(9600);
+    //play_melody();
     Wire.begin(9);
     Wire.onReceive(receiveEvent);
     pinMode(SOUND_PIN, OUTPUT);
     //pinMode(ENABLE_PIN, OUTPUT);
 }
 
+/**
+ * Loop
+ */
 void loop() {
-    //play_melody();
     play(step_position);
     //digitalWrite(ENABLE_PIN, HIGH);
     //delay(1000);
@@ -28,10 +56,13 @@ void loop() {
     //delay(1000);
 }
 
-// tone(pin, frequency(Hz), [duration(ms)]) & notone(pin)
+
+/**
+ * Toggle the tone corresponding to the current step position
+ * tone(pin, frequency(Hz), [duration(ms)]) & notone(pin)
+ */
 void play(int step_position) {
     if (step_position == current) { return; }
-    // toggle the note corresponding to this laser beam
     if (step_position >= 0 && step_position < 12){
         tone(SOUND_PIN, frequencies[step_position], 5000);
         current = step_position;
@@ -43,19 +74,23 @@ void play(int step_position) {
     } 
 }
 
-// Receive integer from the other Arduino.
-// If step_position is between 0 and 11: play the corresponding tone i.
-// If step_position is between 12 and 23: stop playing the corresponding tone (i-12).
+
+/**
+ * Receive integer from the other Arduino.
+ * If step_position is between 0 and 11: play the corresponding tone i.
+ * If step_position is between 12 and 23: stop playing the corresponding tone (i-12).
+ */
 void receiveEvent(int howMany) {
   step_position = Wire.read();    // receive byte as an integer
   //Serial.println(step_position);
 }
 
 
+/**
+ * simple test of the tone() function
+ */
 void play_melody(){
-    // simple test of the tone() function
-    // tone(pin, frequency(Hz), [duration(ms)]) & notone(pin)
-    int melody[] = {NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_F5, NOTE_G5, NOTE_G5, NOTE_G5, NOTE_G5, NOTE_A5};
+    int melody[] = {NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4, NOTE_C5, NOTE_D5, NOTE_E5};
     for (int i = 0; i < 10; i++){
         tone(SOUND_PIN, frequencies[i], note_duration);
         tone(SOUND_PIN, frequencies[i], note_duration);
@@ -65,10 +100,16 @@ void play_melody(){
 }
 
 
+/**
+ * not relevant
+ */
 void play2(int step_position) {
     tone2(SOUND_PIN, frequencies[step_position], note_duration);
 }
 
+/**
+ * not relevant
+ */
 void tone2(int SOUND_PIN, int frequency, int note_duration){
     if (frequency == 0.0){
         delayMicroseconds(note_duration);
